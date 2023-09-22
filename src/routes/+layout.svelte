d47284e00a18f84a6eee5c8e8dbd01e920377b6d
Arnaud F. <25860666+arnaudfl@users.noreply.github.com>
2023-09-22 20:23:48 +0200

    feat: first implementation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Header from './Header.svelte';
+    import '@picocss/pico'
+    import './app.css'
+  </script>
+
+	<Header />
+
+	<main class="container">
+		<slot />
+	</main>
+
+    <footer class="container">
+        <small
+          >Built with <a href="https://picocss.com">Pico</a> •
+          <a href="https://github.com/picocss/examples/blob/master/v1-preview/index.html"
+            >Source code</a
+          ></small
+        >
+      </footer>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class="container">
+    <hgroup>
+      <h1>Pico</h1>
+      <h2>A starter example with all elements and components used in Pico design system.</h2>
+    </hgroup>
+</header>
